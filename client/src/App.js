e03872f41ffcd17cc62c9975cf80de46df52e2b4
Mya <136588892+MariaAksamit@users.noreply.github.com>
2024-07-23 10:38:05 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
-import React from "react";
-import { Outlet, useNavigate, useState } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Outlet, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Dropdown from 'react-bootstrap/Dropdown';
 import Container from "react-bootstrap/Container";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import './App.css';
+import { useCategory } from './routes/SelectedCategoryContext';
 
 function App() {
   let navigate = useNavigate();
+  const [categoryList, setCategoryList] = useState([]);
+  const { selectedCategory, setCategory } = useCategory();
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/category/list')
+       .then(response => response.json())
+       .then(data => setCategoryList(data))
+       .catch(error => console.error('Error fetching category data:', error));
+   }, []); 
 
   return (
     <div className="App">
@@ -30,15 +42,39 @@ function App() {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="/recipeList">  Recepty  </Nav.Link>
-                <Nav.Link href="/ingredientList">  Suroviny   </Nav.Link>
+                <NavDropdown title="Recepty" id="navbarScrollingDropdown">
+                  <NavDropdown.Item 
+                      onClick={() => {
+                        setCategory(null);
+                        navigate("/recipeList")
+                      }}
+                    >
+                      Všechny recepty
+                    </NavDropdown.Item>
+                    <Dropdown.Divider />
+                    {categoryList
+                      .sort((a, b) => a.name.localeCompare(b.name)) // Zoradenie podľa abecedy
+                      .map((category) => (
+                    <NavDropdown.Item
+                      key={category.id}
+                      onClick={() => {
+                        setCategory(category);
+                        navigate("/recipeList?id=" + category.id);
+                      }}
+                      >
+                      {category.name}
+                    </NavDropdown.Item>
+                     ))}
+                </NavDropdown>
+                <Nav.Link onClick={() => navigate("/ingredientList")}>  Suroviny   </Nav.Link>
+                <Nav.Link onClick={() => navigate("/categoryList")}>  Kategorie   </Nav.Link>
               </Nav>
 
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
       </Navbar>
-  
+
       <Outlet />
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -5,21 +5,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { CategoryProvider } from './routes/SelectedCategoryContext';
 import HomeR from './routes/HomeR';
 import RecipeListR from './routes/RecipeListR';
 import IngredientListR from './routes/IngredientListR';
+import CategoryListR from "./routes/CategoryListR";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <CategoryProvider> {/* Zabalte kořenový komponent do poskytovatele kontextu */}
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="" element={<HomeR />} />
           <Route path="recipeList" element={<RecipeListR />} />
           <Route path="ingredientList" element={<IngredientListR />} />
+          <Route path="categoryList" element={<CategoryListR />} />
         </Route>
       </Routes>
+    </CategoryProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
